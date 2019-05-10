93d5e7792c47c3b8ffdb4fff3c1255a2f2908208
biggsz123 <50237986+biggsz123@users.noreply.github.com>
2019-05-10 09:08:45 -0400

    Pokemon pakage

--- Pokemon Weed Green/src/pokemonList/Garchomp.java
+++ Pokemon Weed Green/src/pokemonList/Garchomp.java
@@ -3,9 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pokemon.weed.green;
+package pokemonList;
 
 import java.util.Random;
+import pokemon.weed.green.Pokemon;
 
 /**
  *
